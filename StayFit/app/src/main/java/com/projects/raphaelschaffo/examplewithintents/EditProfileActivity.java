0b19333548718cc18ca11ed27019beafb6242f35
Gygi Damien <damien.gygi@he-arc.ch>
2016-10-25 11:06:32 +0200

    Changement d'interface de l'edition du profil

--- StayFit/app/src/main/java/com/projects/raphaelschaffo/examplewithintents/EditProfileActivity.java
+++ StayFit/app/src/main/java/com/projects/raphaelschaffo/examplewithintents/EditProfileActivity.java
@@ -4,20 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     /* Component Declaration */
     private Button btnProOk;
-    private EditText etProBirthDate;
+    private DatePicker etProBirthDate;
     private EditText etProSize;
     private EditText etProWeight;
     private RadioButton rbtnProMen;
     private RadioButton rbtnProWomen;
     private Switch swProMobileData;
+    private RadioGroup rbtngGender;
+    private NumberPicker npSize;
+    private NumberPicker npWeightKG;
+    private NumberPicker npWeight;
 
     /* Intent OnCreate Method*/
     @Override
@@ -27,18 +34,20 @@ public class EditProfileActivity extends AppCompatActivity {
 
         /* Component Initialisation*/
         btnProOk = (Button)findViewById(R.id.btnProOk);
-        etProBirthDate= (EditText)findViewById(R.id.etProBirthDate);
-        etProSize= (EditText)findViewById(R.id.etProSize);
-        etProWeight=(EditText)findViewById(R.id.etProWeight);
+        etProBirthDate= (DatePicker) findViewById(R.id.etProBirthDate);
         rbtnProMen = (RadioButton)findViewById(R.id.rbtnProMen);
         rbtnProWomen =(RadioButton)findViewById(R.id.rbtnProWomen);
-
+        rbtngGender= (RadioGroup) findViewById(R.id.rbtngGender);
+        npSize = (NumberPicker) findViewById(R.id.npSize);
+        npWeight = (NumberPicker) findViewById(R.id.npWeight);
+        npWeightKG = (NumberPicker) findViewById(R.id.npWeightKG);
+        npWeight.setMaxValue(9);
+        npWeightKG.setMaxValue(200);
+        npSize.setMaxValue(210);
         btnProOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(!etProBirthDate.getText().equals("")&&!etProSize.getText().equals("") && !etProSize.getText().equals(""))
-                {
                     if(rbtnProWomen.isChecked() == true || rbtnProMen.isChecked()==true)
                     {
                         /* Request to create a new User */
@@ -48,7 +57,6 @@ public class EditProfileActivity extends AppCompatActivity {
                         setResult(RESULT_OK);
                         finish();
                     }
-                }
 
             }
         });
