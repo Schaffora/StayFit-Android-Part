9e53029c406af6a63234156e2dfe1db629f5173d
Schaffora <schaffora@gmail.com>
2016-11-08 11:00:28 +0100

    User Interface Part II

--- StayFit/app/src/main/java/stayfit/OnGoingActivity.java
+++ StayFit/app/src/main/java/stayfit/OnGoingActivity.java
@@ -2,6 +2,7 @@ package stayfit;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class OnGoingActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,6 +39,14 @@ public class OnGoingActivity extends FragmentActivity implements OnMapReadyCallb
         txtVOnGoingSpeedAverage=(TextView)findViewById(R.id.txtVOnGoingSpeedAverage);
         txtVOnGoingSteps =(TextView)findViewById(R.id.txtVOnGoingSteps);
 
+        /* Stop Chronometer */
+        btnOnGoingStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrnmtOnGoingCrono.stop();
+                finish();
+            }
+        });
 
          /* Chronometer Start*/
         chrnmtOnGoingCrono.start();
@@ -63,8 +73,15 @@ public class OnGoingActivity extends FragmentActivity implements OnMapReadyCallb
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(-33.866, 151.195);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Your Positon").snippet("Actual Position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addPolyline(new PolylineOptions().geodesic(true)
+                .add(new LatLng(-33.866, 151.195))
+                .add(new LatLng(-33.864, 151.193))
+                .add(new LatLng(-33.866, 151.198))
+                .add(new LatLng(-33.867, 151.203))
+        );
     }
 }
