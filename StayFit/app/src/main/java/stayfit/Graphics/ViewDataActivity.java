70e6ad1385b792fed4c8521991087e565d8c4142
damien.gygi <gygi.damien@gmail.com>
2016-11-15 08:31:51 +0100

    ajout de la derniere activité (vide pour l'instant) et des intens pour y accéder

---
+++ StayFit/app/src/main/java/stayfit/Graphics/ViewDataActivity.java
@@ -0,0 +1,13 @@
+package stayfit.Graphics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(stayfit.R.layout.activity_view_data);
+    }
+}
