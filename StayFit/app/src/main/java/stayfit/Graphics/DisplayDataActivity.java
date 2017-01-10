9b1fc6533c08745d53829309bb5c9c2c0cc19697
Gygi Damien <damien.gygi@he-arc.ch>
2017-01-10 10:59:32 +0100

    view activity

---
+++ StayFit/app/src/main/java/stayfit/Graphics/DisplayDataActivity.java
@@ -0,0 +1,15 @@
+package stayfit.Graphics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import stayfit.R;
+
+public class DisplayDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_data);
+    }
+}
