975bb270445b024966d05bb50aa3f73d4a1a4177
Gygi Damien <damien.gygi@he-arc.ch>
2017-01-16 11:38:42 +0100

    recuperation sample

--- StayFit/app/src/main/java/stayfit/Graphics/DisplayDataActivity.java
+++ StayFit/app/src/main/java/stayfit/Graphics/DisplayDataActivity.java
@@ -1,8 +1,10 @@
 package stayfit.Graphics;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import stayfit.DataBase.DataSample;
 import stayfit.R;
 
 public class DisplayDataActivity extends AppCompatActivity {
@@ -11,5 +13,11 @@ public class DisplayDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_data);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        DataSample dataSample = null ;
+        dataSample = (DataSample)extras.getSerializable("dataSample");
+//        Toast.makeText( getApplicationContext(), dataSample.ID, Toast.LENGTH_LONG).show();
     }
 }
