10bc2832be2dd10aa8807b24ef336aa4d42e800b
Gygi Damien <damien.gygi@he-arc.ch>
2017-01-17 12:11:58 +0100

    ajout d'un carte avec trajet bidon

--- StayFit/app/src/main/java/stayfit/Graphics/DisplayDataActivity.java
+++ StayFit/app/src/main/java/stayfit/Graphics/DisplayDataActivity.java
@@ -1,20 +1,39 @@
 package stayfit.Graphics;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import stayfit.DataBase.DataSample;
 import stayfit.R;
 
-public class DisplayDataActivity extends AppCompatActivity {
+public class DisplayDataActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks,LocationListener {
 
     private TextView txtVMoy;
     private TextView txtFootSteps;
     private TextView txtCal;
     private TextView txtDist;
+    private GoogleMap mapMap;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +62,65 @@ public class DisplayDataActivity extends AppCompatActivity {
         txtFootSteps.setText(dataSample.Steps+"");
         txtCal.setText(dataSample.Calories+"");
         txtDist.setText(dataSample.Distance+"");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        mapMap.addPolyline(new PolylineOptions().geodesic(true)
+                .add(new LatLng(-33.866, 151.195))
+                .add(new LatLng(-33.864, 151.193))
+                .add(new LatLng(-33.866, 151.198))
+                .add(new LatLng(-33.867, 151.203))
+        );
+        LatLng sydney = new LatLng(-33.866, 151.195);
+        mapMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14));
+        mapMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            return;
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i("Connection failed", connectionResult.getErrorMessage());
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("Connection suspended", String.valueOf(i));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
