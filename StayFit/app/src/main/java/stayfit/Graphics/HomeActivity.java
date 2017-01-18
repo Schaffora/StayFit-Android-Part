4da5f769798d102ecfc98ad27469f408cedb917f
Schaffora <schaffora@gmail.com>
2017-01-18 12:15:19 +0100

    Save

--- StayFit/app/src/main/java/stayfit/Graphics/HomeActivity.java
+++ StayFit/app/src/main/java/stayfit/Graphics/HomeActivity.java
@@ -1,16 +1,25 @@
 package stayfit.Graphics;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import stayfit.DataBase.DataSample;
@@ -27,6 +36,14 @@ public class HomeActivity extends AppCompatActivity {
     private RadioButton rbtnHomeRide;
     private RadioGroup rbtnActivity;
 
+    /* Back result Tools */
+    private final int ACTIVITY_RESULT = 100;
+
+    /* Lists*/
+    private List<User> users;
+    private List<DataSample> dataSamples;
+    private List<String> DATABASE;
+
     /* Intent OnCreate*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +51,10 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(stayfit.R.layout.activity_home);
 
         /* DataBase tools*/
-        List<User> users = null;
-        List<DataSample> dataSamples = null ;
+        users = new ArrayList<User>();
+        dataSamples = new ArrayList<DataSample>();
         String actualUser ="";
+        DATABASE = new ArrayList<String>();
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -89,7 +107,7 @@ public class HomeActivity extends AppCompatActivity {
                 {
                     intent.putExtra("ChoosenActivity", "ride");
                 }
-                startActivityForResult(intent, 0);
+                startActivityForResult(intent, ACTIVITY_RESULT);
             }
         });
 
@@ -120,5 +138,102 @@ public class HomeActivity extends AppCompatActivity {
         bundle.putSerializable("users",(Serializable)users);
         bundle.putSerializable("dataSamples",(Serializable)dataSamples);
         intent.putExtras(bundle);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        switch(requestCode){
+            case 100:
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(this, "New user was created. ", Toast.LENGTH_LONG).show();
+                    //DataBaseRefresh();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    public void DataBaseRefresh()
+    {
+        Context context = getApplicationContext();
+        try {
+
+            InputStream inputStream = context.openFileInput("DATABASE.txt");
+            users.clear();
+            dataSamples.clear();
+            DATABASE.clear();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            DATABASE.add(line);
+
+            while (line !=null)
+            {
+                try {
+                    line =reader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                DATABASE.add(line);
+            }
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for(int i=0; i<DATABASE.size();i++)
+            {
+                if (DATABASE.get(i) != null) {
+                    DataBaseInterpret(DATABASE.get(i));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public void DataBaseInterpret(String line)
+    {
+        if(line!=null)
+        {
+
+            String datas=line.substring(1,line.length()-1);
+            List <String> dataTypes = Arrays.asList(datas.split("\\s*=\\s*"));
+            List<String> values =Arrays.asList(dataTypes.get(1).split("\\s*;\\s*"));
+
+            if(dataTypes.get(0).equals("user"))
+            {
+                users.add(new User(Integer.parseInt(values.get(0)),values.get(1),values.get(2),values.get(3),Integer.parseInt(values.get(4)),Integer.parseInt(values.get(5)),values.get(6),values.get(7)));
+            }
+
+            if(dataTypes.get(0).equals("datasample"))
+            {
+                List<String> lats = new ArrayList<String>();
+                List<String> longs = new ArrayList<String>();
+
+                for(int i=8; i<values.size(); i++)
+                {
+                    if(values.get(i) !=null)
+                    {
+                        String[]LatLong= values.get(i).split("/");
+                        lats.add(LatLong[0]);
+                        longs.add(LatLong[1]);
+                    }
+
+                }
+                dataSamples.add(new DataSample(Integer.parseInt(values.get(0)),Integer.parseInt(values.get(1)),Integer.parseInt(values.get(2)),values.get(3),Integer.parseInt(values.get(4)),Integer.parseInt(values.get(5)),Integer.parseInt(values.get(6)),Integer.parseInt(values.get(7)),lats,longs));
+            }
+            else{}
+
+        }
+
+
     }
 }
